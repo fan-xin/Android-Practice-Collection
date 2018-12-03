362afc6a81f56cfc36ae23605eb9528ed3e725ee
fan-xin <fan.xin@jp.fujitsu.com>
2018-12-03 17:33:24 +0900

    将客户端的代码移植到安卓端 运行时需要客户端和服务端在同一个网络中

--- SocketDemo/src/main/java/com/fanxin/android/socketdemo/MainActivity.java
+++ SocketDemo/src/main/java/com/fanxin/android/socketdemo/MainActivity.java
@@ -2,12 +2,74 @@ package com.fanxin.android.socketdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.fanxin.android.socketdemo.biz.UdpClientBiz;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mEtMsg;
+    private Button mBtnSend;
+    private TextView mTvContent;
+
+    //初始化业务类
+    private UdpClientBiz udpClientBiz = new UdpClientBiz();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+
+        mBtnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //获取输入的内容是
+                String msg = mEtMsg.getText().toString();
+                //判断输入信息是否为空
+                if (TextUtils.isEmpty(msg)){
+                    return;
+
+                }
+
+                //加入自己的message
+                appendMsgToContent("client: "+msg);
+
+
+                udpClientBiz.sendMsg(msg, new UdpClientBiz.OnMsgReturnedListener() {
+                    @Override
+                    public void onMsgReturned(String msg) {
+                        //如果有返回的信息，则把信息加入到textview中
+                        appendMsgToContent("server: "+msg);
+
+                    }
+
+                    @Override
+                    public void onError(Exception e) {
+                        e.printStackTrace();
+
+                    }
+                });
+            }
+        });
+    }
+
+    private void appendMsgToContent(String msg){
+        mTvContent.append(msg+"\n");
+    }
+
+    private void initView() {
+
+        mEtMsg = (EditText)findViewById(R.id.id_edittext);
+        mBtnSend = (Button)findViewById(R.id.id_send_button);
+        mTvContent = (TextView)findViewById(R.id.id_tv_content);
+
+
     }
 }
---
+++ SocketDemo/src/main/java/com/fanxin/android/socketdemo/biz/UdpClientBiz.java
@@ -0,0 +1,109 @@
+package com.fanxin.android.socketdemo.biz;
+
+import android.os.Handler;
+import android.os.Looper;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+import java.util.logging.LogRecord;
+
+/**
+ * Created by Fan Xin <fanxin.hit@gmail.com>
+ * 18/12/03  15:03
+ */
+public class UdpClientBiz {
+
+    private String mServerIp = "127.0.1.1";
+    private int mServerPort = 7777;
+    private InetAddress mServerAddress;
+    private DatagramSocket mSocket;
+
+    private Handler mUIHander = new Handler(Looper.getMainLooper());
+
+
+    public UdpClientBiz(){
+
+        try {
+            mServerAddress = InetAddress.getByName(mServerIp);
+            mSocket = new DatagramSocket();
+
+        } catch (SocketException e) {
+            e.printStackTrace();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public interface OnMsgReturnedListener{
+        void onMsgReturned(String msg);
+        void onError(Exception e);
+
+    }
+
+    public void sendMsg(final String msg, final OnMsgReturnedListener listerner){
+
+        //网络交互操作，要在子线程中进行
+        new Thread(){
+            @Override
+            public void run() {
+                try {
+                    byte[] clientMsgBytes = msg.getBytes();
+
+                    DatagramPacket clientPacket =
+                            new DatagramPacket(clientMsgBytes,
+                                    clientMsgBytes.length,
+                                    mServerAddress,
+                                    mServerPort);
+                    //向服务器端发送数据
+                    mSocket.send(clientPacket);
+
+                    //接收服务端的数据
+                    byte[] buf = new byte[1024];
+                    DatagramPacket serverMsgPacket =
+                            new DatagramPacket(buf,buf.length);
+                    mSocket.receive(serverMsgPacket);
+
+                    //已经接收到信息
+                    InetAddress address = serverMsgPacket.getAddress();
+                    int port = serverMsgPacket.getPort();
+                    final String serverMsg = new String(serverMsgPacket.getData(),0,
+                            serverMsgPacket.getLength());
+                    System.out.println(" message is "+serverMsg);
+
+                    mUIHander.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            listerner.onMsgReturned(serverMsg);
+                        }
+                    });
+
+
+
+                } catch (final IOException e) {
+                    mUIHander.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            listerner.onError(e);
+                        }
+                    });
+                }
+            }
+
+        }.start();
+
+    }
+
+    public void onDestory(){
+        if (mSocket!= null){
+            mSocket.close();
+        }
+    }
+
+}
