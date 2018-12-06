a888df90569b2158199ee4e9c2e3592ed6a6e55c
fan-xin <fan.xin@jp.fujitsu.com>
2018-12-06 12:08:36 +0900

    完善代码

--- ServiceDemo/src/main/java/com/fanxin/android/servicedemo/MyService.java
+++ ServiceDemo/src/main/java/com/fanxin/android/servicedemo/MyService.java
@@ -76,57 +76,6 @@ public class MyService extends Service {
 
         //对于onBind方法而言，要求返回IBinder对象
         //实际上，我们会自己定义一个内部类，继承Binder类
-
-
-//        return new IBinder() {
-//            @Nullable
-//            @Override
-//            public String getInterfaceDescriptor() throws RemoteException {
-//                return null;
-//            }
-//
-//            @Override
-//            public boolean pingBinder() {
-//                return false;
-//            }
-//
-//            @Override
-//            public boolean isBinderAlive() {
-//                return false;
-//            }
-//
-//            @Nullable
-//            @Override
-//            public IInterface queryLocalInterface(@NonNull String descriptor) {
-//                return null;
-//            }
-//
-//            @Override
-//            public void dump(@NonNull FileDescriptor fd, @Nullable String[] args) throws RemoteException {
-//
-//            }
-//
-//            @Override
-//            public void dumpAsync(@NonNull FileDescriptor fd, @Nullable String[] args) throws RemoteException {
-//
-//            }
-//
-//            @Override
-//            public boolean transact(int code, @NonNull Parcel data, @Nullable Parcel reply, int flags) throws RemoteException {
-//                return false;
-//            }
-//
-//            @Override
-//            public void linkToDeath(@NonNull DeathRecipient recipient, int flags) throws RemoteException {
-//
-//            }
-//
-//            @Override
-//            public boolean unlinkToDeath(@NonNull DeathRecipient recipient, int flags) {
-//                return false;
-//            }
-//        };
-
     }
 
     class MyBinder extends Binder{
