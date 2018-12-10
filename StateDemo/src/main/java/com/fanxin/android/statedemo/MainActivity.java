52b6ad4a3a6b099eac6f56300d77665ae252b488
fan-xin <fan.xin@jp.fujitsu.com>
2018-12-10 15:27:17 +0900

    创建布局，显示时间和文本框

--- StateDemo/src/main/java/com/fanxin/android/statedemo/MainActivity.java
+++ StateDemo/src/main/java/com/fanxin/android/statedemo/MainActivity.java
@@ -2,12 +2,29 @@ package com.fanxin.android.statedemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
+    private long createTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //真实的项目，时间要从服务器获取
+        createTime = System.currentTimeMillis();
+
+        String formatTime = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").format(new Date(this.createTime));
+
+        TextView mCreateTimeTextView = (TextView) findViewById(R.id.id_tv_createTime);
+        mCreateTimeTextView.setText(formatTime);
+
+
+
     }
 }
